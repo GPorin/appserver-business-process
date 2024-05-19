e0a3f8a77e49521815d91eaa30182db21401428e
Якушев Архип <yakushevomgtu@gmail.com>
2024-05-19 15:30:36 +0600

    Added QueueCommandConsumerAdapetr

---
+++ appserver/server/QueueCommandConsumerAdapter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+
+namespace AppServer.Commands
+{
+    public class QueueCommandConsumerAdapter: ICommandConsumer
+    {
+        private BlockingCollection<ICommand> _queue;
+        public QueueCommandConsumerAdapter(BlockingCollection<ICommand> queue)
+        {
+            _queue = queue;
+        }
+
+        public void Receive(ICommand cmd)
+        {
+            _queue.TryAdd(cmd);
+        }
+    }
+}
