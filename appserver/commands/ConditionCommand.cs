d504879c4f85e51e285d293bd35872ee0f0cfb90
Якушев Архип <yakushevomgtu@gmail.com>
2024-05-19 15:14:38 +0600

    Added ConditionCommand

---
+++ appserver/commands/ConditionCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppServer.Commands
+{
+    public class ConditionCommand: ICommand
+    {
+        private readonly ICommand _condition;
+        private readonly ICommand _thenCommand;
+        private readonly ICommand? _elseCommand;
+
+        public ConditionCommand(ICommand condition, ICommand thenCommand, ICommand? elseCommand = null)
+        {
+            _condition = condition;
+            _thenCommand = thenCommand;
+            _elseCommand = elseCommand;
+        }
+
+        public void Execute()
+        {
+            try
+            {
+                _condition.Execute();
+            }
+            catch (Exception)
+            {
+                _elseCommand?.Execute();
+                return;
+            }
+            _thenCommand.Execute();
+        }
+    }
+}
