e35b0843bf7cb49165a5f6844c4ff62681cd4ad0
Якушев Архип <yakushevomgtu@gmail.com>
2024-06-06 19:34:16 +0600

    Started second test

--- businessprocess/grammarparser.test/GrammarTest.cs
+++ businessprocess/grammarparser.test/GrammarTest.cs
@@ -1,4 +1,6 @@
 using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+using Microsoft.Win32;
 
 namespace grammarparser.test
 {
@@ -15,6 +17,25 @@ namespace grammarparser.test
             CommonTokenStream tokens = new CommonTokenStream(lexer);
             ProcessParser parser = new ProcessParser(tokens);
             Assert.Equal(0, parser.NumberOfSyntaxErrors);
+        }
+
+        [Fact]
+
+        public void ListenerGetsCommandQueue() 
+        {
+            string pathToFile = "../../../../fileparsers.test/parsedString.txt";
+            string inputString = File.ReadAllText(pathToFile);
+
+            AntlrInputStream input = new AntlrInputStream(inputString);
+            ProcessLexer lexer = new ProcessLexer(input);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            ProcessParser parser = new ProcessParser(tokens);
+
+
+
+            IParseTree tree = parser.process();
+            ProcessSimpleCommandListener listener = new ProcessSimpleCommandListener();
+            ParseTreeWalker.Default.Walk(listener, tree);
         }
     }
 }
